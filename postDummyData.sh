bf555c95693eb4893f8405e0eb12d6063eab84d4
mdvanes <4253562+mdvanes@users.noreply.github.com>
2023-06-09 15:55:12 +0200

    get key from cli

--- postDummyData.sh
+++ postDummyData.sh
@@ -2,22 +2,26 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-APPSYNC_PREFIX="j"
-AUTH_KEY="eyJ"
+APPSYNC_URL=$(amplify status | grep "GraphQL endpoint" | cut -d " " -f 3)
+APPSYNC_API_KEY=$(amplify status | grep "GraphQL API KEY" | cut -d " " -f 4)
 
 function put () {
+    # List current items
+    curl -X POST -H "Content-Type:application/graphql" -H "x-api-key:$APPSYNC_API_KEY" -d '{ "query": "query { listLoras { items {time} } }" }' "$APPSYNC_URL"
+    echo -e "\n\n"
+
     # curl -X POST http://localhost:3000/api/lora \
     # -H 'Content-Type: application/json' -H 'Origin: http://example.com' \
     # -d "[{ \"bn\": \"urn:dev:DVNUUID:???:\", \"bt\": 111111111111},{ \"n\": \"battery\", \"u\": \"%\", \"vs\": $3},{ \"n\": \"accelerationX\", \"u\": \"m/s2\", \"v\": 0},{ \"n\": \"latitude\", \"u\": \"lat\", \"v\": $1},{ \"n\": \"longitude\", \"u\": \"lon\", \"v\": $2},{ \"n\": \"locTime\", \"vs\": \"1\"}, { \"n\": \"temperature\", \"v\": $4, \"u\": \"Cel\"} ]"
 
-    TIME=$(date +"%Y%m%d%_H%M%S")
+    # TIME=$(date +"%Y%m%d%_H%M%S")
 
-    curl "https://$APPSYNC_PREFIX.appsync-api.eu-west-1.amazonaws.com/graphql" \
-    -H "authorization: $AUTH_KEY" \
-    --data-raw "{\"query\":\"mutation CreateLora(\$input: CreateLoraInput\\u0021, \$condition: ModelLoraConditionInput) {\\n  createLora(input: \$input, condition: \$condition) {\\n    id\\n    time\\n    lat\\n    long\\n    temp\\n    createdAt\\n    updatedAt\\n    owner\\n  }\\n}\\n\",\"variables\":{\"input\":{\"time\":$TIME,\"lat\":$1,\"long\":$2,\"temp\":$4}}}"
+    # APPSYNC_API_KEY works for query but not for mutation, not even in AppSync GraphQL web console!  It gives Unauthorized
+    # curl -XPOST -H "Content-Type:application/graphql" -H "x-api-key:$APPSYNC_API_KEY" --data-raw "{\"query\":\"mutation CreateLora(\$input: CreateLoraInput\\u0021, \$condition: ModelLoraConditionInput) {\\n  createLora(input: \$input, condition: \$condition) {\\n    id\\n    time\\n    lat\\n    long\\n    temp\\n    createdAt\\n    updatedAt\\n    owner\\n  }\\n}\\n\",\"variables\":{\"input\":{\"time\":$TIME,\"lat\":$1,\"long\":$2,\"temp\":$4}}}" "$APPSYNC_URL"
 }
 
 # curl http://localhost:3000/api/coords
+curl -X POST -H "Content-Type:application/graphql" -H "x-api-key:$APPSYNC_API_KEY" -d '{ "query": "query { listLoras { items {time} } }" }' "$APPSYNC_URL"
 
 put "51.9073121" "4.489056" "85.0" "21"
 
